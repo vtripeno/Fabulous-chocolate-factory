3ceaf697e8e84566dc42faba2e9746787be4a427
Victor Yuri Alves Tripeno <vtripeno@gmail.com>
2025-08-25 19:48:06 -0300

    implements coroutine and paralelism

--- src/main/kotlin/br/com/store/entrypoint/api/StartChocolateProcess.kt
+++ src/main/kotlin/br/com/store/entrypoint/api/StartChocolateProcess.kt
@@ -1,10 +1,8 @@
 package br.com.store.entrypoint.api
 
-import java.time.Duration
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
 import org.springframework.http.MediaType
-import org.springframework.http.codec.ServerSentEvent
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
@@ -12,16 +10,46 @@ import reactor.core.publisher.Flux
 @RestController
 class StartChocolateProcess {
 
-    @GetMapping("/sse-flow", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun startChocolateFlow(): Flux<String> {
-        return Flux.just(
-            "Starting chocolate production process...",
-            "Mixing ingredients...",
-            "Heating the mixture...",
-            "Pouring into molds...",
-            "Cooling down...",
-            "Packaging chocolates...",
-            "Chocolate production process completed!"
-        ).delayElements(Duration.ofSeconds(2))
+    @GetMapping("/start-chocolate-production-flow", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+suspend fun startChocolateFlowCoroutine(): Flux<String> = Flux.create { sink ->
+
+    kotlinx.coroutines.runBlocking {
+            val mix = async {
+                delay(2000)
+                "Mixing ingredients..."
+            }
+
+            val heat = async {
+                delay(3000)
+                "Heating the mixture..."
+            }
+
+            // mold depende de mix + heat
+            val mold = async {
+                mix.await()
+                heat.await()
+                delay(1000)
+                "Pouring into molds..."
+            }
+
+            val cool = async {
+                mold.await()
+                delay(2000)
+                "Cooling down..."
+            }
+
+            val pack = async {
+                cool.await()
+                delay(1000)
+                "Packaging chocolates..."
+            }
+
+            listOf(mix, heat, mold, cool, pack).forEach { job ->
+                sink.next(job.await())
+            }
+
+            sink.next("Chocolate production completed!")
+            sink.complete()
+        }
     }
 }
